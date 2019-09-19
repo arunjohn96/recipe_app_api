901ee40458548b522bca17d9886e65ed95872d9d
root <arjkizhakkedathu@gmail.com>
2019-09-19 12:34:07 +0530

    configures postgre Db

--- app/core/tests/tests_admin.py
+++ app/core/tests/tests_admin.py
@@ -1,23 +1,23 @@
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
-from django.urls import reverse 
+from django.urls import reverse
 
 
 class AdminSiteTests(TestCase):
-    
+
     def setUp(self):
-        self.client =  Client()
+        self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email = 'admin@admin.com',
-            password = 'pass123'
+            email='admin@admin.com',
+            password='pass123'
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email = 'test@admin.com',
-            password = 'pass123',
-            name = 'test user full name'
+            email='test@admin.com',
+            password='pass123',
+            name='test user full name'
         )
-    
+
     def test_for_users_listed(self):
         """ Test that users are listed on user page """
         url = reverse('admin:core_user_changelist')
