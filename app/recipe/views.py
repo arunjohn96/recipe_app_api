1127f3bdcb3e4a14ed3f5fd8142a89e828119d58
root <arjkizhakkedathu@gmail.com>
2019-09-23 13:21:44 +0530

    added list retrieval feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag, Ingredients
+from core.models import Tag, Ingredients, Recipe
 from recipe import serializers
 
 
@@ -32,3 +32,15 @@ class IngredientsViewSet(BaseRecipeAttrViewSet):
     """ Manage the ingredients in the database """
     queryset = Ingredients.objects.all()
     serializer_class = serializers.IngredientSerializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """ manage recipes in the database """
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """ Retrieve the recipes for the authenticated user """
+        return self.queryset.filter(user=self.request.user)
